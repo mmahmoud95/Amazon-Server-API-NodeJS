afe88548f8861acd2e2cf67f54b8559ba826bf0f
mustafamhmod95@gmail.com <mustafamhmod95@gmail.com>
2023-11-09 15:46:45 +0200

    switch to db atlas

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 
 const dbConnect = () => {
     mongoose
-        // .connect(
-        //     "mongodb+srv://mustafamhmod95:bYrUCQ2Jf6z3ZU8w@amazon.fj4moo9.mongodb.net/Amazon"
-        // )
-        .connect("mongodb://127.0.0.1:27017/Amazon")
+        .connect(
+            "mongodb+srv://mustafamhmod95:bYrUCQ2Jf6z3ZU8w@amazon.fj4moo9.mongodb.net/Amazon"
+        )
+        // .connect("mongodb://127.0.0.1:27017/Amazon")
         .then(() => {
             console.log("connected to Amazon_Data_Base successfully");
         })
