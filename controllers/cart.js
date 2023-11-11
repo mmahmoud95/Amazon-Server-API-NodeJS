4b825437bc230cc3b3863a99fd2d613a2b6c028a
mustafamhmod95@gmail.com <mustafamhmod95@gmail.com>
2023-11-11 22:12:47 +0200

    update total price function

--- controllers/cart.js
+++ controllers/cart.js
@@ -22,8 +22,11 @@ const addNewCart = async (req, res) => {
     let cart = await cartModel.findOne({ userId: userId });
     try {
         if (!cart) {
-            const newCart = await cartModel.create(cartItems);
-            calcTotalCartPrice(cart);
+            const newCart = await cartModel.create({
+                ...cartItems,
+                totalPrice: product.price,
+            });
+            // calcTotalCartPrice(newCart);
             res.status(201).json({
                 userId,
                 message: "Cart created successfully",
