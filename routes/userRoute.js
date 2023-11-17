f08bbbd493cc868cadc5469d6bc3d363c121f3aa
Randa-10 <randa010011012@gmail.com>
2023-11-17 21:43:09 +0200

    admin login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const {signUp,logIn,deleteUser,updateUser, updateUserById, CheckEmail, updateUserAddressById}=require('../controllers/userController')
+const {signUp,logIn,logInAdmin,deleteUser,updateUser, updateUserById, CheckEmail, updateUserAddressById}=require('../controllers/userController')
 const{auth}=require('../middlewares/userAuth')
 const{authRole}=require('../middlewares/adminAuth')
 
@@ -8,7 +8,7 @@ const{authRole}=require('../middlewares/adminAuth')
 router.post('/signup',signUp)
 router.post('/login',logIn)
 router.post('/checkEmail',CheckEmail)
-
+router.post('/loginAdmin',logInAdmin)
 // delete user by his id  if you are admin
 router.delete('/login/delete',auth,authRole,deleteUser)
 // update your first name and last name
